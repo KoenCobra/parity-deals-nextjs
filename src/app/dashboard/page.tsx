f956519d61c2103ce099cbe1eed1bc1be71754ad
Koen De Groot <koendegroot84@gmail.com>
2024-11-04 21:35:51 +0100

    Revert "added forms"
    This reverts commit f922296d959f3b4a5ee5d75ef8c37e09cb95a0d5.

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,6 @@
-import { getProducts } from "@/server/db/products";
-import { auth } from "@clerk/nextjs/server";
 import React from "react";
-import { NoProducts } from "./_components/NoProducts";
 
-const DashboardPage = async () => {
-  const { userId, redirectToSignIn } = await auth();
-  if (userId == null) return redirectToSignIn();
-
-  const products = await getProducts(userId, { limit: 6 });
-
-  if (products.length === 0) return <NoProducts />;
-
+const DashboardPage = () => {
   return <div>DashboardPage</div>;
 };
 
